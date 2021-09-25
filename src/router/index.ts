859f7e687d581f34cc06c58464bde7638254e37d
g-zhima <bilibili3322@gmail.com>
2021-09-26 00:04:07 +0800

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,25 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import About from "../views/About.vue";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import About from '../views/About.vue'
+import Home from '../views/Home.vue'
 
 const routes: RouteRecordRaw[] = [
   {
-    path: "/",
-    redirect: "/home",
+    path: '/',
+    redirect: '/home',
   },
   {
-    path: "/home",
+    path: '/home',
     component: Home,
   },
   {
-    path: "/about",
+    path: '/about',
     component: About,
   },
-];
+]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-});
+})
 
-export default router;
+export default router
